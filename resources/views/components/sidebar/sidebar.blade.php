25f938d8e88114ffb79f5dce3aa1ad201c491ca4
Tahmid Ferdous <tahmid.tf1@gmail.com>
2026-07-09 16:01:30 +0600

    Assets and sidebar added

---
+++ resources/views/components/sidebar/sidebar.blade.php
@@ -0,0 +1,39 @@
+  <div class="sidenav-menu">
+      <div class="nav accordion" id="accordionSidenav">
+          <!-- Sidenav Menu Heading (Account)-->
+          <!-- * * Note: * * Visible only on and above the sm breakpoint-->
+          <div class="sidenav-menu-heading d-sm-none">Account</div>
+          <!-- Sidenav Link (Alerts)-->
+          <!-- * * Note: * * Visible only on and above the sm breakpoint-->
+          <a class="nav-link d-sm-none" href="#!">
+              <div class="nav-link-icon"><i data-feather="bell"></i></div>
+              Alerts
+              <span class="badge bg-warning-soft text-warning ms-auto">4 New!</span>
+          </a>
+          <!-- Sidenav Link (Messages)-->
+          <!-- * * Note: * * Visible only on and above the sm breakpoint-->
+          <a class="nav-link d-sm-none" href="#!">
+              <div class="nav-link-icon"><i data-feather="mail"></i></div>
+              Messages
+              <span class="badge bg-success-soft text-success ms-auto">2 New!</span>
+          </a>
+          <!-- Sidenav Menu Heading (Core)-->
+          <div class="sidenav-menu-heading">Core</div>
+          <!-- Sidenav Accordion (Dashboard)-->
+          <a class="nav-link collapsed" href="javascript:void(0);" data-bs-toggle="collapse"
+              data-bs-target="#collapseDashboards" aria-expanded="false" aria-controls="collapseDashboards">
+              <div class="nav-link-icon"><i data-feather="activity"></i></div>
+              Dashboards
+              <div class="sidenav-collapse-arrow">
+                  <i class="fas fa-angle-down"></i>
+              </div>
+          </a>
+          <div class="collapse" id="collapseDashboards" data-bs-parent="#accordionSidenav">
+              <nav class="sidenav-menu-nested nav accordion" id="accordionSidenavPages">
+                  <a class="nav-link" href="dashboard-1.html">Default</a>
+                  <a class="nav-link" href="dashboard-2.html">Multipurpose</a>
+                  <a class="nav-link" href="dashboard-3.html">Affiliate</a>
+              </nav>
+          </div>
+      </div>
+  </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,10 +10,9 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+require __DIR__ . '/auth.php';
+require __DIR__ . '/profile.php';
 
-require __DIR__.'/auth.php';
+Route::get('/test', function () {
+    return view('layouts.admin');
+});
